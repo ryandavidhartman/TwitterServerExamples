6cc184eb663ceb845293bb20f2b41c7ccec79693
ryandavidhartman <ryandavidhartman@gmail.com>
2022-03-22 21:11:31 -0400

    finishing up quick start

--- FinagleExamples/quickstart/src/main/scala/Client.scala
+++ FinagleExamples/quickstart/src/main/scala/Client.scala
@@ -2,19 +2,38 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http
 import com.twitter.util.{Await, Future}
 
+/**
+ * run with
+ * $ ./sbt 'runMain Client'
+ */
+
 object Client extends App {
-  //#builder
+  /**
+   * In our server example, we define a Service to respond to requests.
+   * Clients work the other way around: we’re given a Service to use.
+   *
+   * Just as we exported services with the Http.serve, method, we can
+   * import them with a Http.newService, giving us an instance of
+   * Service[http.Request, http.Response]:
+   */
   val client: Service[http.Request, http.Response] = Http.client
     .withTransport.tls("scala-lang.org")
     .newService("scala-lang.org:443")
-  //#builder
-  //#dispatch
+
+
+  /**
+   * client is a Service to which we can dispatch an http.Request and in
+   * return receive a Future[http.Response] — the promise of an http.Response
+   * (or an error) some time in the future. We furnish newService with the target
+   * of the client: the host or set of hosts to which requests are dispatched.
+   */
   val request = http.Request(http.Method.Get, "/")
   request.host = "www.scala-lang.org"
   val response: Future[http.Response] = client(request)
-  //#dispatch
-  //#callback
-  Await.result(response.onSuccess { rep: http.Response => println("GET success: " + rep) })
 
-  //#callback
+  /**
+   * Now that we have response, a Future[http.Response], we can register a
+   * callback to notify us when the result is ready:
+   */
+  Await.result(response.onSuccess { rep: http.Response => println("GET success: " + rep) })
 }
--- FinagleExamples/quickstart/src/main/scala/Proxy.scala
+++ FinagleExamples/quickstart/src/main/scala/Proxy.scala
@@ -2,7 +2,13 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.Await
 
-//#app
+/**
+ * Now we’re ready to create an HTTP proxy! Notice the symmetry above:
+ * servers provide a Service, while a client uses it.
+ * Indeed, an HTTP proxy can be constructed by just replacing the service
+ * we defined in the Server example with one that was imported with a Http.newService:
+ */
+
 object Proxy extends App {
   val client: Service[Request, Response] =
     Http.newService("twitter.com:80")
@@ -10,4 +16,4 @@ object Proxy extends App {
   val server = Http.serve(":8080", client)
   Await.ready(server)
 }
-//#app
+
